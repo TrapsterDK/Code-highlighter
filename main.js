2310b92673ac21a31b75ec0369fd8ca85249a0f8
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2022-05-24 22:41:33 +0200

    Semi working

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+//https://jsrepos.com/lib/isagalaev-highlight-js-javascript-code-highlighting
+// language list hljs.listLanguages()
+
+//api https://highlightjs.readthedocs.io/en/latest/api.html
+
 function highlight_code(){
+    $('code').text($('#scode').val())
+    hljs.highlightAll();
+    console.log(hljs.listLanguages())
     return false;
-}
+} 
