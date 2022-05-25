13967d3e0b5f8887ccd8afbef783db93de620b07
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2022-05-25 11:23:34 +0200

    Library change to Prism.js, view code

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 //https://jsrepos.com/lib/isagalaev-highlight-js-javascript-code-highlighting
-// language list hljs.listLanguages()
-
+// Prism.languages
 //api https://highlightjs.readthedocs.io/en/latest/api.html
 
-function highlight_code(){
-    $('code').text($('#scode').val())
-    hljs.highlightAll();
-    console.log(hljs.listLanguages())
+//https://github.com/PrismJS/prism/issues/1424
+
+//https://github.com/PrismJS/prism/blob/master/plugins/autoloader/prism-autoloader.js
+
+//https://github.com/PrismJS/prism/issues/1881
+/*function highlight(code, language) {
+	if (Prism.languages[language]) {
+		return Prism.highlight(code, Prism.languages[language], language);
+	} else {
+		return Prism.util.encode(code);
+	}
+}
+
+function highlight_code(input = null){
+    if(input == null) input = $('#scode').val()
+    let highlight = Prism.highlight(input, Prism.languages.javascript, 'javascript')
+    console.log(highlight)
+    $('code').html(highlight)
     return false;
-} 
+}
+
+window.onload = () => {
+    highlight_code($('#scode').attr('placeholder'))
+};*/
+/*
+#include &lt;stdio.h&gt;
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+*/
