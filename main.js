6ca115b24ebdd2fc48cd4a45e5cbc4e55f1067f9
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2022-05-25 17:40:11 +0200

    javascript highlight not working

--- main.js
+++ main.js
@@ -6,8 +6,10 @@
 
 //https://github.com/PrismJS/prism/blob/master/plugins/autoloader/prism-autoloader.js
 
+//https://cdn.jsdelivr.net/npm/prismjs@1.28.0/components/
+
 //https://github.com/PrismJS/prism/issues/1881
-/*function highlight(code, language) {
+function highlight(code, language) {
 	if (Prism.languages[language]) {
 		return Prism.highlight(code, Prism.languages[language], language);
 	} else {
@@ -18,14 +20,15 @@
 function highlight_code(input = null){
     if(input == null) input = $('#scode').val()
     let highlight = Prism.highlight(input, Prism.languages.javascript, 'javascript')
-    console.log(highlight)
+    console.log(Prism.plugins.autoloader.languages_path)
     $('code').html(highlight)
     return false;
 }
 
 window.onload = () => {
     highlight_code($('#scode').attr('placeholder'))
-};*/
+};
+
 /*
 #include &lt;stdio.h&gt;
 int main() {
