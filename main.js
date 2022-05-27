aa2de01eeb52e076f4345a863e386cd62c3170d3
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2022-05-27 21:21:37 +0200

    Theme testing

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
+/*
+TODO FIXES
+fonts copy paste into word determin size, not possible at the moment
+Xonokai break-word
+Line number breaking differently should be the same
+*/
+
 //https://prismjs.com/
 
 //supported languages
 //https://prismjs.com/components.js
+
+//https://stackoverflow.com/questions/118241/calculate-text-width-with-javascript/5047712#5047712
+/**
+  * Uses canvas.measureText to compute and return the width of the given text of given font in pixels.
+  * 
+  * @param {String} text The text to be rendered.
+  * @param {String} font The css font descriptor that text is to be rendered with (e.g. "bold 14px verdana").
+  * 
+  * @see https://stackoverflow.com/questions/118241/calculate-text-width-with-javascript/21015393#21015393
+  */
+function getTextWidth(text, font) {
+    // re-use canvas object for better performance
+    const canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+    const context = canvas.getContext("2d");
+    context.font = font;
+    const metrics = context.measureText(text);
+    return metrics.width;
+}
+
+function getCssStyle(element, prop) {
+    return window.getComputedStyle(element, null).getPropertyValue(prop);
+}
+
+function getCanvasFontSize(el = document.body) {
+    const fontWeight = getCssStyle(el, 'font-weight') || 'normal';
+    const fontSize = getCssStyle(el, 'font-size') || '16px';
+    const fontFamily = getCssStyle(el, 'font-family') || 'Times New Roman';
+
+    return `${fontWeight} ${fontSize} ${fontFamily}`;
+}
 
 //https://github.com/PrismJS/prism/issues/1881
 //highlighting function that checks if grammar exists and encodes it based on that
@@ -56,6 +93,7 @@ $(document).ready(function() {
 
     $("#highlight").click(function() {
         highlight_code($('#input-code').val())
+        console.log($('code > span').css('font-size'))
     });
 
     window.set_theme_from_id($('#theme-selector').val())
